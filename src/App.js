168f26b557c8619f3bf4b3dc1979d6a179c14746
Noella Hirwa <noellahirwa@Noellas-MacBook-Pro.local>
2021-12-10 12:33:45 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import AddForm from './components/AddForm';
-import SmurfList from './components/SmurfList';
-import Header from './components/Header';
+import { fetchSmurfs } from "./actions";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AddForm from "./components/AddForm";
+import SmurfList from "./components/SmurfList";
+import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-const App = ()=> {
-  return (
-    <div className="App">
-      <Header />
+class App extends Component {
+  componentDidMount() {
+    console.log("didMount");
+    this.props.dispatch(fetchSmurfs());
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
 
-      <main>
-        <SmurfList/>
-        <AddForm/>
-      </main>
-    </div>
-  );
+        <main>
+          <SmurfList />
+          <AddForm />
+        </main>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null)(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import reducer from "./reducers";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import "./index.css";
 import App from "./App";
 
-const { worker } = require('./mocks/browser');
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const { worker } = require("./mocks/browser");
 worker.start();
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 //Task List:
